8bf428550ad2bdb9d8c68fe02855974ca08c99c5
akexiu <akexiuw@163.com>
2020-03-07 23:16:27 +0800

    Python正则表达式，第一天结束

--- 第一章：正则表达式/Re.py
+++ 第一章：正则表达式/Re.py
@@ -1,5 +1,9 @@
 #正则表达式
 import re
+from random import randrange,choice
+from string import ascii_lowercase as ls
+from sys import maxsize
+from time import  ctime
 
 
 c = re.compile('as')
@@ -39,3 +43,37 @@ print(re.findall('a','banana'))
 print(re.finditer(r'(c\w+)','china check',re.I).__next__().group(1))
 
 """1.3.12 2020-3-7明天继续"""
+"""使用sub和subn搜索与替换"""
+#第三个参数中包含第一个参数的值替换成第二个参数，大小写有区别
+print(re.sub("[abc]","D","abcdefabcd"))
+print(re.subn("[abc]","D","abcdefabcd"))#替换并且返回替换的总数量
+
+"""复杂的字符串分割 split分割符"""
+print(re.split(":","a:b:c"))
+DATE = ("Linux","Windows","Apple","YouSee")
+for date in DATE:
+    print(re.split(", |(?=(?:\d{4}|[A-Z]{2}))",date))
+    #r"(?i)" 忽略大小写
+    # r"(?s)"表示（.）号区分大小写
+    # r"(?x)" 该标允许用户通过抑制在正则表达式中使用空白符来创建更加易懂的表达式，
+print(re.findall(r"(?x)yes","/yes \/yesa. YES||"))
+
+"""读取文件"""
+f=open("C:\\Users\\Administrator\\Desktop\\git基本操作.txt","r")
+for ls in f:
+    print(ls)
+f.close()
+
+"""正则表达式练习数据生成器 以下生产有点问题"""
+tlds =('com','edu','net','org','gov')
+for i in range(randrange(5,11)):
+    dtint = randrange(maxsize)
+    dtstr = ctime(dtint)
+    llen =  randrange(4,8)
+    login = "a".join(choice(ls) for j in range(llen))
+    dlen = randrange(llen,13)
+    dom = "c".join(choice(ls) for j in range(dlen))
+    print("%s::%s@%s.%s::%d-%d-%d" % (dtstr,login,dom,choice(tlds),dtint,llen,dlen))
+    print(dom)
+
+"""明天继续：第二章，网络编程，2020-3-7"""
